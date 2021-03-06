93cdf0f5145e6cd9957e1b3badaa81f68db16495
Denis Malinin <70207084+Melowetty@users.noreply.github.com>
2021-03-06 11:37:57 +0500

    Wlliam Charts test's

--- app/src/main/java/com/melowetty/investment/StockView.kt
+++ app/src/main/java/com/melowetty/investment/StockView.kt
@@ -1,25 +1,15 @@
 package com.melowetty.investment
 
-import android.R
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
-import im.dacer.androidcharts.LineView
+import android.os.Bundle
+import com.db.williamchart.view.LineChartView
 
 
 class StockView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stock_view)
-        val lineView = findViewById<View>(R.id.line_view) as LineView
-        lineView.setDrawDotLine(false) //optional
-        lineView.
-
-        lineView.setShowPopup(LineView.SHOW_POPUPS_MAXMIN_ONLY) //optional
-
-        lineView.setBottomTextList(strList)
-        lineView.setColorArray(intArrayOf(Color.BLACK, Color.GREEN, Color.GRAY, Color.CYAN))
-        lineView.setDataList(dataLists) //or lineView.setFloatDataList(floatDataLists)
+        val lineChart = findViewById<LineChartView>(R.id.lineChart)
 
     }
 }
