e3acd10f312df6af541cd5a561b66b1d03de4609
Denis Malinin <70207084+Melowetty@users.noreply.github.com>
2021-03-17 17:28:42 +0500

    refractor

--- app/src/main/java/com/melowetty/investment/MainActivity.kt
+++ app/src/main/java/com/melowetty/investment/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.melowetty.investment
 
 import android.content.Context
-import android.content.Intent
 import android.os.AsyncTask
 import android.os.Bundle
-import android.util.Log
-import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
--- app/src/main/java/com/melowetty/investment/managers/ApiManager.java
+++
@@ -1,5 +0,0 @@
-package com.melowetty.investment.managers;
-
-public class ApiManager {
-
-}
