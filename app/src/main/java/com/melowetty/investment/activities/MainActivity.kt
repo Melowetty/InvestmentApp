afc34a8e2a8d199e2a4d2396b0fd3844c04a45e7
Denis Malinin <70207084+Melowetty@users.noreply.github.com>
2021-04-03 20:57:16 +0500

    optimize imports

--- app/src/main/java/com/melowetty/investment/activities/MainActivity.kt
+++ app/src/main/java/com/melowetty/investment/activities/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.melowetty.investment.activities
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
